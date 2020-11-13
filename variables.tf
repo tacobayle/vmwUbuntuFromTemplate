498b6c4104602843fe4eb747fe85359bc2a6303e
Nicolas Bayle <nicolas.bayle.it@gmail.com>
2020-11-13 10:24:53 +0000

    20201113

--- variables.tf
+++ variables.tf
@@ -14,7 +14,7 @@ variable "vcenter" {
     dc = "sof2-01-vc08"
     cluster = "sof2-01-vc08c01"
     datastore = "sof2-01-vc08c01-vsan"
-    resource_pool = "Cluster-1/Resources"
+    resource_pool = "sof2-01-vc08c01/Resources"
     network = "vxw-dvs-34-virtualwire-3-sid-1080002-sof2-01-vc08-avi-mgmt"
   }
 }
